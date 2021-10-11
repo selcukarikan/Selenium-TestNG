0559bfca1be94e16da099883c2a86a5e44dee2f5
Selcuk Arikan <selcukarikan1301@gmail.com>
2021-10-11 20:59:07 +0200

    Priority

--- src/test/java/tests/day07/C01_Priority.java
+++ src/test/java/tests/day07/C01_Priority.java
@@ -3,7 +3,11 @@ package tests.day07;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class C01_Priority {
     // 3 test methodu olusturun
@@ -16,7 +20,30 @@ public class C01_Priority {
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
+    }
+    @Test(priority = 3)
+    public void amazonTest(){
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getTitle());
+
+    }
+
+    @Test(priority = 1)
+    public void techproTest(){
+        driver.get("https://www.techproeducation.com");
+        System.out.println(driver.getTitle());
+    }
+    @Test(priority = 2)
+    public void faceTest(){
+        driver.get("https://www.facebook.com");
+        System.out.println(driver.getTitle());
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
 
     }
 
