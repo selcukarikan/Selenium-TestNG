a5f2d082ed4e69e15dfacae32662589e04fd4cd3
Selcuk Arikan <selcukarikan1301@gmail.com>
2021-10-14 22:03:30 +0200

    IFrame

--- src/test/java/tests/day09/C02_Iframe.java
+++ src/test/java/tests/day09/C02_Iframe.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,19 +32,35 @@ public class C02_Iframe {
     }
     //    ● Bir metod olusturun: iframeTest
     @Test
-    public void iframeTest(){
+    public void iframeTest()  {
 
         WebElement baslikYaziElementi= driver.findElement(By.tagName("h3"));
         Assert.assertTrue(baslikYaziElementi.isEnabled(),"baslik yazisi erisebilir degil");
         //		○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve  konsolda 	yazdirin.
         System.out.println(baslikYaziElementi.getText());
-
-        driver.switchTo().frame("mce_0_ifr");
+        //		○ Text Box’a “Hello Welt!!” yazin.
+        WebElement iFrame= driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+        driver.switchTo().frame(iFrame);
         // driver.switchTo().frame(0); index numarasi ile de yazilabilir
         WebElement schreibText= driver.findElement(By.xpath("//*[@id='tinymce']"));
-        //		○ Text Box’a “Merhaba Dunya!” yazin.
+
         schreibText.clear();
-        schreibText.sendKeys("Hello Welt");
+        schreibText.sendKeys("Hello Welt!");
+
+        //bir iframe e gecis yaptiktan sonra yeniden anasayfa ile ilgili islem yapmak istersek
+        //gecis yaptigimiz Iframe e geri donmeliyiz
+
+        driver.switchTo().defaultContent();
+
+        //	TextBox’in altinda bulunan “Elemental Selenium” linkini textinin gorunur oldugunu
+        //	dogrulayin ve  konsolda yazdirin.
+        WebElement elemantalLink= driver.findElement(By.linkText("Elemantal Selenium"));
+        SoftAssert softAssert=new SoftAssert();
+
+        softAssert.assertTrue(elemantalLink.isDisplayed());
+        softAssert.assertAll();
+        System.out.println(elemantalLink.getText());
+
     }
     @AfterClass
     public void tearDown(){
