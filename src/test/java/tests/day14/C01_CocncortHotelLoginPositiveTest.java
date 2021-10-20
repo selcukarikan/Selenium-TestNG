b25387e391a56d1e89cb9369d4621c9c358436bd
Selcuk Arikan <selcukarikan1301@gmail.com>
2021-10-20 14:02:51 +0200

    ConcortHotel

---
+++ src/test/java/tests/day14/C01_CocncortHotelLoginPositiveTest.java
@@ -0,0 +1,14 @@
+package tests.day14;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C01_CocncortHotelLoginPositiveTest extends TestBase {
+    @Test
+    public void positiveLoginTest(){
+
+        driver.get("https://www.concorthotel.com/");
+
+    }
+
+}
