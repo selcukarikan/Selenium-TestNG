d39ce9cd6fdb7207b6208e02721de0f6b1ffae8e
Selcuk Arikan <selcukarikan1301@gmail.com>
2021-10-20 22:41:32 +0200

    Faker

--- src/test/java/tests/day12/C01_ExplicitlyWaitTest.java
+++ src/test/java/tests/day12/C01_ExplicitlyWaitTest.java
@@ -1,16 +1,17 @@
 package tests.day12;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import utilities.TestBase;
 
 public class C01_ExplicitlyWaitTest extends TestBase {
-
-
-
-
 
     @Test
     public void implicitlyWaitTest(){
@@ -36,12 +37,16 @@ driver.get("https://the-internet.herokuapp.com/dynamic_controls");
 driver.get("https://the-internet.herokuapp.com/dynamic_controls");
         //4. Remove butonuna basin.
         driver.findElement(By.xpath("//button[@onclick='swapCheckbox()']")).click();
-
         //5. "It's gone!" mesajinin goruntulendigini dogrulayin.
+        WebDriverWait wait=new WebDriverWait(driver,23);
+        //expilicity mit locate
+        WebElement itsGone=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
+        Assert.assertTrue(itsGone.isDisplayed());
 
         //6. Add buttonuna basin
-
+        driver.findElement(By.xpath("//button[@onclick='swapCheckbox()']")).click();
         //7. It's back mesajinin gorundugunu test edin
+        WebElement itsBack=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
 
     }
 }
