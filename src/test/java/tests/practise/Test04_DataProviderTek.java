2f8b6982f3e73afb2780db6c40a1c249b660ce87
selcukarikan <selcukarikan1301@gmail.com>
2021-11-19 21:41:32 +0100

    DataProviderCift

--- src/test/java/tests/practise/Test04_DataProviderTek.java
+++ src/test/java/tests/practise/Test04_DataProviderTek.java
@@ -1,12 +1,26 @@
 package tests.practise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
 public class Test04_DataProviderTek  extends TestBase {
 
-    @Test
-    public void test(){
+        @DataProvider
+        public static Object[] aranacakKelimeler() {
+            String data [] = {"Java", "JavaScript", "Pyhton"};
+            return data;
+        }
 
+        @Test (dataProvider = "aranacakKelimeler")
+        public void test(String aranacakKelime){
+            driver.get("https://www.gittigidiyor.com/");
+
+            WebElement aramaKutusu = driver.findElement(By.xpath("//input[@name='k']"));
+
+            aramaKutusu.sendKeys(aranacakKelime +Keys.ENTER);
+        }
     }
-}
